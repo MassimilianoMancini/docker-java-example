af03c9d30fb790e9bcbf4ea204f7d1c7ab94db97
MassimilianoMancini <massimiliano1.mancini@gmail.com>
2021-08-14 10:13:02 +0200

    First commit

---
+++ hellodocker/src/test/java/com/examples/hellodocker/AppTest.java
@@ -0,0 +1,17 @@
+package com.examples.hellodocker;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void shouldAnswerWithTrue()
+    {
+        assertThat(true);
+    }
+}
